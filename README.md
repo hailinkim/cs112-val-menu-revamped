b38cef75cbbf2a32dac5d227c083405eb01811f9
hailinkim <haikim20@amherst.edu>
2021-11-16 15:37:16 -0500

    Added menu

---
+++ README.md
@@ -0,0 +1,13 @@
+Title: Val Menu Revamped
+
+Authors: Mia Jung (BO), Vanesa Farooq (VF), Hailin Kim (HK)
+
+Date: November 29, 2021
+
+Course: CS-112
+
+Files used: "Val Menu".csv
+
+Software used: IntelliJ
+
+Abstract: For our project, we put the menu meal data from AC Nutrition into a text file and sorted it by calorie, fat, protein, and carb count, type of meal (breakfast, lunch, or dinner), whether the meal is vegetarian, allergen, or vegan-friendly meal, and by different dietary needs (i.e. heavy protein meals for athletes or low-fat meals). 
--- src/ValMenu.java
+++ src/ValMenu.java
@@ -11,16 +11,22 @@ public class ValMenu {
         char option;
         do{
             System.out.println("\nMenu: ");
-            System.out.print("\n1. " +
-                    " \n2. " +
-                    " \n3. " +
+            System.out.print("\n1. Program Description" +
+                    " \n2. Authors" +
+                    " \n3. Run the program" +
                     "\n0. exit program."+
                     "\n\nEnter an option (1-3, 0 to quit): ");
             option = cin.next().charAt(0); // returns the character at index 0.
             cin.nextLine();
             switch(option){
-                case '1': //dosomething; break;
-                case '2': //dosomething; break;
+                case '1':
+                    System.out.println("This program was designed for CS112 final project." +
+                            "The program's database is based on Val menu scraped from AC Nutrition website." +
+                            "It allows users to choose Breakfast/Lunch/Dinner menu based on their dietary preferences and restrictions.");
+                    break;
+                case '2':
+                    System.out.println("This program was designed by Amherst College sophomores, Mia Jung, Vanesa Farooq, and Angelica Kim.");
+                    break;
                 case '0': System.out.println("\n** You entered option 0 to exit. Good bye! **"); break;
                 default: System.out.println("\n** Input error! Enter a valid input, please! **");
             }
