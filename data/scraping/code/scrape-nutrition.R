c573ea6dd72a2a91b857b993a4d158f29f46bdf6
hailinkim <haikim20@amherst.edu>
2021-11-28 00:53:37 -0500

    Scraped salad bar menu from AC Nutrition website

--- data/scraping/code/scrape-nutrition.R
+++ data/scraping/code/scrape-nutrition.R
@@ -1,8 +1,22 @@
 library(RSelenium)
-browser <- rsDriver(port = 1234L, browserName = "firefox")
+library(rvest)
+browser <- remoteDriver(port = 1236L, browserName = "firefox")
 browser$open()
 browser$navigate("https://acnutrition.amherst.edu/NetNutrition/1#")
-elem <- browser$findElement(using = "css", 'table.cbo_nn_sideUnitTable')
+elem <- browser$findElement(using = "css", '.cbo_nn_itemHover')$getElementText()
+html <- browser$getPageSource()[[1]]
+
+salad_scraped <- read_html(html) %>% 
+  html_nodes("table.cbo_nn_itemGridTable") %>% 
+  .[[1]] %>% 
+  html_table(fill=T)
+write_csv(salad_scraped, "salad_scraped.csv")
+
+i <- read_html(html) %>% 
+  html_nodes(".cbo_nn_itemHover") %>% 
+  html_attr("onkeyup")
+
+
 menu3 <- elem %>% 
   html_table()
 library(XML)
@@ -17,8 +31,3 @@ menu3 <- read_html(html) %>%
 doc <- htmlParse(browser$getPageSource()[[1]])
 readHTMLTable(doc)
 
-library(readr)
-library(dplyr)
-t <- read_csv("/Users/angelica/Desktop/insurance.csv") 
-t <- t %>% 
-  select(-version)
